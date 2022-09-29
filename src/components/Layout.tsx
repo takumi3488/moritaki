d911047d784777191cd5c81e0da6b0f0aa9ed33e
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-09-29 15:22:45 +0900

    mainレイアウト修正

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,11 +11,11 @@ const Layout: FC<Props> = ({ children, title }) => {
   return (
     <div className="max-w-8xl mx-auto px-4 sm:px-6 md:px-8">
       <div className="min-h-screen flex flex-col lg:flex-row items-center lg:items-stretch max-w-5xl mx-auto gap-6">
-        <main className="pt-4 flex-1 w-full">
-          <h1 className='text-xl md:text-2xl lg:text-3xl text-center font-bold mb-12'>
-            {createTitle(title)}
+        <main className="pt-4 flex-1 w-full pb-12">
+          <h1 className='text-xl md:text-2xl lg:text-3xl text-center font-bold'>
+            もりた記
           </h1>
-          {!title && <p className='mb-8 text-center text-xs md:text-sm'>-書評とか-</p>}
+          <p className='text-center text-xs md:text-sm mb-8'>-書評とか-</p>
           {children}
         </main>
         <Aside />
